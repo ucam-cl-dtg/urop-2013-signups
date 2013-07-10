5453bbcf7945719b9e9fdb6415398eab313460ba
Mistral CONTRASTIN <mistralcontrastin@dhcp-172-17-155-149.eduroam.lapwing.private.cam.ac.uk>
2013-07-10 16:28:35 +0100

    add user model

--- src/main/java/uk/ac/cam/signups/models/User.java
+++ src/main/java/uk/ac/cam/signups/models/User.java
@@ -5,6 +5,17 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.ws.rs.FormParam;
 
+@Entity
+@Table(name="USERS")
 public class User {
-
+	private String crsid;
+	
+	public User() {}
+	
+	public User(String crsid) {
+		this.crsid = crsid;
+	}
+	
+	public String getCrsid() {return crsid;}
+	public void setCrsid(String crsid) {this.crsid = crsid;}
 }
