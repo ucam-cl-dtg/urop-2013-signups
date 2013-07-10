0637bac5b78b50c5d8f6e0fe2614f410a9fc6b27
Holly <hollyanne00@googlemail.com>
2013-07-10 13:32:28 +0100

    webauth stuff

---
+++ src/main/java/uk/ac/cam/signups/util/HibernateSessionRequestFilter.java
@@ -0,0 +1,66 @@
+package uk.ac.cam.signups.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HibernateSessionRequestFilter implements Filter {
+
+	public static final String SESSION_FACTORY = HibernateSessionRequestFilter.class
+			.getName() + ".SessionFactory";
+
+	private static Logger log = LoggerFactory
+			.getLogger(HibernateSessionRequestFilter.class);
+
+	private SessionFactory sf;
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		log.debug("Initializing filter...");
+		sf = configureSessionFactory();
+	}
+
+	private static SessionFactory configureSessionFactory()
+			throws HibernateException {
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		ServiceRegistry serviceRegistry = new ServiceRegistryBuilder()
+				.applySettings(configuration.getProperties())
+				.buildServiceRegistry();
+		SessionFactory sessionFactory = configuration
+				.buildSessionFactory(serviceRegistry);
+		return sessionFactory;
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		request.setAttribute(SESSION_FACTORY, sf);
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+	public static Session openSession(HttpServletRequest request) {
+		SessionFactory factory = (SessionFactory) request
+				.getAttribute(SESSION_FACTORY);
+		return factory.openSession();
+	}
+}
