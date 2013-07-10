3be7670a9dedcb9be009cdf8c31ff8684735377d
Mistral CONTRASTIN <mistralcontrastin@dhcp-172-17-155-149.eduroam.lapwing.private.cam.ac.uk>
2013-07-10 17:19:46 +0100

    change event type

--- src/main/java/uk/ac/cam/signups/models/Event.java
+++ src/main/java/uk/ac/cam/signups/models/Event.java
@@ -19,7 +19,7 @@ public class Event {
 	@Id
 	@GeneratedValue(generator="increment")
 	@GenericGenerator(name="increment", strategy = "increment")
-	private String id;
+	private int id;
 	
 	// private Set<Deadline> deadlines;
 	
@@ -29,7 +29,7 @@ public class Event {
 		this.id = id;
 	}
 	
-	public String getId() {return id;}
+	public int getId() {return id;}
 	public void setId(int id) {this.id = id;}
 	
 	/*
