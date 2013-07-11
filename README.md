0efbd8c093ca1f5047e3b7da648904b58bf3c126
Holly <hollyanne00@googlemail.com>
2013-07-11 16:33:14 +0100

    readme and other little things

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-urop-2013-signups
+# signups
 =================
+System to create and signup for events such as supervisions, ticking sessions<br/>
+Features:
+
+## models (uk.ac.cam.signups.util)
+
+## controllers (uk.ac.cam.signups.util)
+
+## util (uk.ac.cam.signups.util)
+### 
--- src/main/java/uk/ac/cam/signups/controllers/HomePageController.java
+++ src/main/java/uk/ac/cam/signups/controllers/HomePageController.java
@@ -128,7 +128,7 @@ public class HomePageController {
 	  	// If no, create them
 	  	if(user==null){
 	  		log.debug("User " + crsid + "does not exist");
-	  		User newUser = new User(crsid);
+	  		User newUser = new User(crsid, null, null, null, null, null);
 	  		session.save(newUser);
 	  		log.info("User " + crsid + " added to USERS table");
 	  	}
