b16484c03f23cd1a272b446b09480b4884ac2dea
Holly <hollyanne00@googlemail.com>
2013-07-12 16:26:28 +0100

    group controller

--- src/main/java/uk/ac/cam/signups/controllers/ApplicationController.java
+++ src/main/java/uk/ac/cam/signups/controllers/ApplicationController.java
@@ -30,20 +30,18 @@ public class ApplicationController {
 	protected String crsid;
 	
 	//UserLookupManager setup
-	protected void initialiseUser(){
+	protected User initialiseUser() {
+		
 		// This will extract the CRSID of the current user and return it:
 		log.debug("Getting crsid from raven");	
 		crsid = (String) request.getSession().getAttribute("RavenRemoteUser");
+		
 		// Create UserLookupManager for this user
 		log.debug("Creating userLookupManager");	
 		ulm = UserLookupManager.getUserLookupManager(crsid);
+		
 		// Add user to database if necessary
 		log.debug("Checking if user is in database");
-		registerUser(crsid);		
-	}
-	
-	// Add to User class later
-	protected void registerUser(String crsid) {
 		// Begin hibernate session
 		log.debug("begin hibernate session");
 		session = HibernateSessionRequestFilter.openSession(request);
@@ -58,10 +56,17 @@ public class ApplicationController {
 	  		User newUser = new User(crsid, null, null, null, null, null, null);
 	  		session.save(newUser);
 	  		log.info("User " + crsid + " added to USERS table");
+		  	log.debug("closing hibernate session");
+			session.getTransaction().commit();
+			session.close(); 
+	  		return newUser;
 	  	}
 	  	// Close hibernate session
 	  	log.debug("closing hibernate session");
 		session.getTransaction().commit();
-		session.close();		
+		session.close();
+		
+		return user;
 	}
+
 }
---
+++ src/main/java/uk/ac/cam/signups/util/HibernateSessionManager.java
@@ -0,0 +1,46 @@
+package uk.ac.cam.signups.util;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.DefaultComponentSafeNamingStrategy;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateSessionManager {
+    private static SessionFactory sf = configureSessionFactory();
+    
+    private static SessionFactory configureSessionFactory()
+            throws HibernateException {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+configuration.setNamingStrategy(new DefaultComponentSafeNamingStrategy());
+        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder()
+                .applySettings(configuration.getProperties())
+                .buildServiceRegistry();
+        SessionFactory sessionFactory = configuration
+                .buildSessionFactory(serviceRegistry);
+        return sessionFactory;
+    }
+    
+    public static SessionFactory getSF() {
+        return sf;
+    }
+    
+    public static Session getSession() {
+     Session session = sf.getCurrentSession();
+    
+     if (session.isOpen()) {
+     session = sf.openSession();
+     }
+    
+        return session;
+    }
+
+    public static Session getTransaction() {
+        Session session = getSession();
+        session.beginTransaction();
+        return session;
+    }
+}
