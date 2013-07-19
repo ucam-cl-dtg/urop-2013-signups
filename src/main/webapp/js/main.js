679f80a034f584430b9b1e46b904fa2b5aeda7a0
Mistral CONTRASTIN <mistralcontrastin@dhcp-172-17-153-198.eduroam.lapwing.private.cam.ac.uk>
2013-07-19 14:41:59 +0100

    js

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,85 +1,88 @@
 $(document).ready(function() {
 	// EVENTS CONTROLLER
-	
-	type_adder(); 	// Add new type 
-	date_adder(); 	// Add new date field
-	toggle_rows();	// Toggle between row types
-	
-	// GROUPS CONTROLLER
-	//Ajax autocomplete lookup user
 
-	//Lookup user token input
-	//Delete a group
-    $("body").on("click", ".group_delete", function() {
-        var group_id = $(this).parents("form").attr("id");
-        // Ajax delete request
-            var deleteData = $.ajax({
-                  type: 'DELETE',
-                  url: "/timetable-signups/groups/" + group_id,
-                  success: function(resultData) {  
-                  // Reload the page for now.. can replace this with just redsiplaying div later?
-                  location.reload();
-                  }
-            });
-    });
-    //Edit a group
-    $("body").on("click", ".group_edit", function() {  
-        var group_id = $(this).parents("form").attr("id");
-        alert(group_id);
-        window.location = "/app/#timetable-signups/groups/" + group_id + "/edit/";
-    });
-	
-	
+	type_adder(); // Add new type
+	date_adder(); // Add new date field
+	toggle_rows(); // Toggle between row types
+
+	// GROUPS CONTROLLER
+	// Ajax autocomplete lookup user
+
+	// Lookup user token input
+	// Delete a group
+	$(".main").on("click", ".group_delete", function() {
+		var group_id = $(this).parents("form").attr("id");
+		// Ajax delete request
+		var deleteData = $.ajax({
+		  type : 'DELETE',
+		  url : "/timetable-signups/groups/" + group_id,
+		  success : function(resultData) {
+			  // Reload the page for now.. can replace this with just redsiplaying div
+				// later?
+			  location.reload();
+		  }
+		});
+	});
+	// Edit a group
+	$(".main").on("click", ".group_edit", function() {
+		var group_id = $(this).parents("form").attr("id");
+		alert(group_id);
+		window.location = "/app/#timetable-signups/groups/" + group_id + "/edit/";
+	});
+
 	// DEADLINES CONTROLLER
 
 	// EVENTS CONTROLLER
 });
 
-// Add new date field 
+// Add new date field
 function date_adder() {
-	$(".datepicker").datepicker({ dateFormat: "dd/mm/yy"});
-	$("#new_slot").click(function(e) {
+	$(".main").on("click","#new_slot", function(e) {
 		e.preventDefault();
 
 		// Create date time picker
 		datetimepicker = "<div>";
 		datetimepicker += "<input class='datepicker' name='available_dates[]'/>"; // Date
-		// Sensible hours 
+		// Sensible hours
 		datetimepicker += "<select name='available_hours[]'>";
-		for(var i = 6; i < 24; i++) {
+		for ( var i = 6; i < 24; i++) {
 			datetimepicker += "<option>" + i + "</option>";
 		}
 		datetimepicker += "</select>:";
 		// Minutes
 		datetimepicker += "<select name='available_minutes[]'>";
-		for(var i = 0; i < 60; i++) {
+		for ( var i = 0; i < 60; i++) {
 			datetimepicker += "<option>" + i + "</option>";
 		}
 		datetimepicker += "</select></div>";
-		
+
 		$(datetimepicker).insertBefore($(this).parent());
-		$(".datepicker").datepicker({ dateFormat: "dd/mm/yy"});
+		$(".datepicker").datepicker({dateFormat : "dd/mm/yy"});
 	});
 }
 
 // Add new type
 function type_adder() {
-	$("#new_type").click(function(e) {
-		e.preventDefault();
-		
-		$("<div><input type='text' name='types[]'></div>").insertBefore($(this).parent());
-	});
+	$(".main").on(
+	    "#new_type",
+	    "click",
+	    function(e) {
+		    e.preventDefault();
+
+		    $("<div><input type='text' name='types[]'></div>").insertBefore(
+		        $(this).parent());
+	    });
 }
 
 // Toggle between row types
 function toggle_rows() {
-	$("input[type='radio']#datetime").click(function() {
+	$(".main").on("click", "input[type='radio']#datetime", function() {
 		$(".manual_rows").slideUp("fast", function() {
 			$(".datetime_rows").slideDown("fast");
 		});
 	});
 
-	$("input[type='radio']#manual").click(function() {
+	$(".main").on("click", "input[type='radio']#manual", function() {
 		$(".datetime_rows").slideUp("fast", function() {
 			$(".manual_rows").slideDown("fast");
 		});
