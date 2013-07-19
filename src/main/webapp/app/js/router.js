2e38d33a35db55c008dfd52e188c54b3d6442c70
Mistral CONTRASTIN <mistralcontrastin@dhcp-172-17-153-198.eduroam.lapwing.private.cam.ac.uk>
2013-07-19 10:14:05 +0100

    trivia

--- src/main/webapp/app/js/router.js
+++ src/main/webapp/app/js/router.js
@@ -1,5 +1,3 @@
-var BASE_PATH = "/";
-
 function Router (routes) {
     var router = new Backbone.Router;
 
@@ -27,7 +25,7 @@ function config (router, route, value) {
         throw new Error("Unsupported type for route: " + route);
 
     return router.route(route, route, function(){
-        loadModule($('.main'), window.location.hash, value);
+        loadModule(window.location.hash, value);
     });
 }
 
@@ -49,18 +47,7 @@ function postModuleLoad () {
 function getLocation(location) {
     if (location[0] == "#")
         location = location.slice(1);
-    return window.location.protocol + "//" + window.location.host + BASE_PATH + location;
-}
-
-function getRouteParams() {
-    var fragment = Backbone.history.fragment,
-         routes = _.map(Backbone.history.handlers, function(x) { return x.route });
-    var matched = _.find(routes, function(handler) {
-        return handler.test(fragment);
-    });
-
-
-    return router._extractParameters(matched, Backbone.history.fragment);
+    return window.location.protocol + "//" + window.location.host + "/" + location;
 }
 
 function getTemplate(name) {
@@ -72,21 +59,7 @@ function getTemplate(name) {
     return res;
 }
 
-function asyncLoad(elems) {
-    elems.each(function(i) {
-       var elem = $(elems[i]),
-           data_path = getLocation(elem.attr("data-path")),
-           template_name = elem.attr("template-name");
-       $.get(data_path, function(json) {
-            applyTemplate(elem, template_name, json);
-       }).fail(function(err) {
-            console.log(err);
-            applyTemplate(elem, template_name, {});
-       })
-    });
-}
-
-function applyTemplate(elem, template, data) {
+function applyTemplate(template, data) {
     var templateFunc;
     if (typeof template == "string") {
         templateFunc = getTemplate(template);
@@ -94,22 +67,21 @@ function applyTemplate(elem, template, data) {
         templateFunc = getTemplate(template(data));
     }
 
-    elem.html(templateFunc(data));
-    asyncLoad(elem.find(".async-loader"));
+    $(".main").html(templateFunc(data));
 }
 
 //
 // template can either be a string with the name of the template
 // or a function that returns the name of the template.
 
-function loadModule(elem, location, template) {
+function loadModule(location, template) {
    var location = getLocation(location);
-
+   $('.main').html('<h3>Loading...</h3>');
    $.get(location, function(data) {
-       applyTemplate(elem, template, data);
+       applyTemplate(template, data);
        postModuleLoad();
    }).fail(function() {
-       elem.html('<h3>Error: could not load ' + location + '</h3>');
+       $('.main').html('<h3>Error: could not load ' + location + '</h3>');
        postModuleLoad();
    });
 }
