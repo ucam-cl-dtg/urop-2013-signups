947c660b77491d9851242b37a870e9594713737e
Holly <hollyanne00@googlemail.com>
2013-07-22 11:37:27 +0100

    changes deadlines model structure

--- src/main/java/uk/ac/cam/signups/controllers/DeadlinesController.java
+++ src/main/java/uk/ac/cam/signups/controllers/DeadlinesController.java
@@ -20,6 +20,9 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.jboss.resteasy.annotations.Form;
 
+import uk.ac.cam.signups.forms.DeadlineForm;
+import uk.ac.cam.signups.forms.GroupForm;
+import uk.ac.cam.signups.helpers.LDAPQueryHelper;
 import uk.ac.cam.signups.models.Deadline;
 import uk.ac.cam.signups.models.Group;
 import uk.ac.cam.signups.models.User;
@@ -33,24 +36,53 @@ import com.googlecode.htmleasy.ViewWith;
 @Path("/signapp/deadlines")
 public class DeadlinesController extends ApplicationController {
 	
+	private User currentUser;
+	
 	// Index 
 	@GET @Path("/") //@ViewWith("deadlines.index")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Map indexDeadlines() {
 		// Get current user
-		User currentUser = initialiseUser();
+		currentUser = initialiseUser();
 		
 
 		return ImmutableMap.of("crsid", currentUser.getCrsid(), "groups", currentUser.getGroupsMap());
 	}
+
+	// Find groups
+	@POST @Path("/queryGroup")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List queryCRSId(String q) {
+		currentUser = initialiseUser();
+		String crsid = currentUser.getCrsid();
+		
+		//Remove q= prefix
+		String x = q.substring(2);
+		
+		//List of group matches
+		ArrayList<ImmutableMap<String,?>> matches = new ArrayList<ImmutableMap<String, ?>>();
+		
+		//Get matching group names.. O(n) each time.. is this too slow? maybe define the .equals and hashCode method?
+		for(Group g : currentUser.getGroups()){
+			if(g.getTitle().contains(x)){
+				matches.add(ImmutableMap.of("group_id", g.getId(), "group_name", g.getTitle()));
+			}
+		}
+		
+		return matches;
+	}
 	
-//	// Create 
-//	@POST @Path("/") 
-//	public void createDeadline(@Form Deadline deadline) {
-//		
-//		throw new RedirectException("/" + deadline.getId());
-//	}
-//	
+	// Create
+	@POST @Path("/") 
+	public void createGroup(@Form DeadlineForm deadlineForm) throws Exception {
+		currentUser = initialiseUser();
+		
+		int id = deadlineForm.handle(currentUser);
+		
+		throw new RedirectException("/app/#signapp/deadlines");
+	}
+	
+
 //	// Edit
 //	@GET @Path("/{id}/edit") @ViewWith("/soy/deadlines.edit")
 //	public Map editDeadline(@PathParam("id") int deadlineId) {
