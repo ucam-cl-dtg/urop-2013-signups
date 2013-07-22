cd8a982562a052b7b0505aeb54749867dd94d183
Mistral CONTRASTIN <mistralcontrastin@dhcp-172-17-153-198.eduroam.lapwing.private.cam.ac.uk>
2013-07-22 17:15:21 +0100

    make holly's templates work

--- src/main/webapp/js/signups.js
+++ src/main/webapp/js/signups.js
@@ -64,6 +64,31 @@ moduleScripts['signapp'] = {
                   }
             });
         });
+        
+        $(".member_token_input").tokenInput("/signapp/groups/queryCRSID", {
+            method: "post",
+            tokenValue: "crsid",
+            propertyToSearch: "crsid",
+            theme: "facebook",
+            minChars: 3,
+            hintText: "Search for a user",
+            resultsLimit: 10,
+            preventDuplicates: true,
+              
+            resultsFormatter: function(item){ return "<li>" + "<div style='display: inline-block; padding-left: 10px;'><div class='full_name'>" + item.name + " (" + item.crsid + ")</div><div class='email'>" + item.crsid + "@cam.ac.uk</div></div></li>" },
+            tokenFormatter: function(item) { return "<li><p>" + item.name + " (" + item.crsid + ")</p></li>" },                           
+        });
+        
+        $(".exgroup_token_input").tokenInput("/signapp/groups/queryGroup", {
+          method: "post",
+          tokenValue: "id",
+          propertyToSearch: "name",
+          theme: "facebook",
+          tokenLimit : 1,
+          
+          resultsFormatter: function(item){ return "<li>" + "<div style='display: inline-block; padding-left: 10px;'><div class='full_name'>" + item.name + "</div><div class='email'>" + item.description + "</div></div></li>" },
+          tokenFormatter: function(item) { return "<li><p>" + item.name + "</p></li>" },
+        });
       }
     ],
 
@@ -72,5 +97,35 @@ moduleScripts['signapp'] = {
 
       }
     ]
+  },
+  
+  'deadlines' : {
+  	'index' : [
+  	  function() {
+        $(".deadline_user_token_input").tokenInput("/signapp/groups/queryCRSID", {
+          method: "post",
+          tokenValue: "crsid",
+          propertyToSearch: "crsid",
+          theme: "facebook",
+          minChars: 3,
+          hintText: "Search for a user",
+          resultsLimit: 10,
+          preventDuplicates: true,
+          
+          resultsFormatter: function(item){ return "<li>" + "<div style='display: inline-block; padding-left: 10px;'><div class='full_name'>" + item.name + " (" + item.crsid + ")</div><div class='email'>" + item.crsid + "@cam.ac.uk</div></div></li>" },
+          tokenFormatter: function(item) { return "<li><p>" + item.name + " (" + item.crsid + ")</p></li>" },                           
+        });
+        
+        $(".deadline_group_token_input").tokenInput("/signapp/deadlines/queryGroup", {
+          method: "post",
+          tokenValue: "group_id",
+          propertyToSearch: "group_name",
+          theme: "facebook",
+          hintText: "Search your groups",
+          resultsLimit: 10,
+          preventDuplicates: true           
+        });
+  	  }        
+  	]
   }
 }
