7f7fd6c46e4da19f19011d6f1f99e192293f7b82
Mistral CONTRASTIN <mistralcontrastin@dhcp-172-17-153-198.eduroam.lapwing.private.cam.ac.uk>
2013-07-23 16:50:36 +0100

    fill form

--- src/main/java/uk/ac/cam/signups/forms/FillSlot.java
+++ src/main/java/uk/ac/cam/signups/forms/FillSlot.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Set;
 import java.util.HashSet;
 
+import uk.ac.cam.signups.models.Row;
 import uk.ac.cam.signups.models.Slot;
 import uk.ac.cam.signups.models.User;
+import uk.ac.cam.signups.models.Type;
 import uk.ac.cam.signups.util.HibernateUtil;
 import uk.ac.cam.signups.util.Util;
 
@@ -29,14 +31,28 @@ public class FillSlot {
 		
 		List<Slot> slots = (List<Slot>) session.createQuery("from Slot as slot where slot.row.event.id = :id").setParameter("id", id).list();
 		
+		int columnsSize = slots.get(0).getRow().getSlots().size();
+		
 		if (ids.equals(Util.getIds(slots))) {
+			Slot slot;
+			Row row;
+			Type type;
 			for(int i = 0; i < slotIds.length; i++) {
 				User owner = User.registerUser(crsids[i]);
 				if (owner == null) continue;
-				Slot slot = Util.findById(slots, slotIds[i]);
+				slot = Util.findById(slots, slotIds[i]);
 				slot.setOwner(owner);
+				int typeId;
 				session.update(slot);
+				if (!((typeId = typeIds[i/columnsSize]) == 0)) {
+					type = (Type) session.createQuery("from Type as type where type.id = :type_id AND type.event.id = :id").setParameter("type_id",typeId).setParameter("id", id).uniqueResult();
+					row = slot.getRow();
+					row.setType(type);
+					session.update(row);
+				}
 			}
+		} else {
+			// TODO unauthorized access
 		}
 	}
 }
