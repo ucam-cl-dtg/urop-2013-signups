97383cd6b6a58d2e6398a2cb57ba3fa769bc81b8
Andrew Rice <acr31@cam.ac.uk>
2013-08-26 10:07:45 +0100

    Pick the port to forward based on the target machine (backups to different machines can run in parallel)

--- backup/src/main/scripts/backup.sh
+++ backup/src/main/scripts/backup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+get_port() {
+    echo $(( `dig +short $1 | sed -e "s/\.//g"` % 55535 + 10000 ))
+}
+
 # this tells bash that if any 'simple' command exits with a non-zero
 # return value the script should die too
 set -e
@@ -10,9 +14,10 @@ USER=signups
 PASSWORD=signups
 BACKUPFILE=backup.sql
 DATE=`date +%Y-%m-%d-%H:%M:%S`
+PORT=`get_port $HOST`
 
-CMD="pg_dump -p 5433 -h localhost -U $USER $DB"
-ssh $HOST -f -L 5433:localhost:5432 sleep 10
+CMD="pg_dump -p $PORT -h localhost -U $USER $DB"
+ssh $HOST -f -L $PORT:localhost:5432 sleep 10
 export PGPASSWORD=$PASSWORD
 $CMD > $BACKUPFILE
 export PGPASSWORD=
